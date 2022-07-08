05113697e60b7ae8e9712a5eeede65631836e511
Akshay-2397 <108930274+Akshay-2397@users.noreply.github.com>
2022-07-08 15:55:24 +0530

    Update Employee.cs

--- Employee.cs
+++ Employee.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
+//ADDED NEW COMMENT
 namespace ORMFrameWork.Models
 {
     [Table("emp10")]
